1290aeb4d18827502a7d0f796d7eb762635b54d9
Rey276 <65110492+Rey276@users.noreply.github.com>
2023-04-26 23:55:32 -0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,32 @@
+<?php 
+
+include "config.php"; 
+
+if (isset($_GET['id'])) {
+
+    $user_id = $_GET['id'];
+
+    $sql = "DELETE FROM `Users` WHERE `id`='$user_id'";
+
+    $result = $conn->query($sql);
+
+    if ($result == TRUE) {
+
+        echo "Record deleted successfully.";
+
+    }else{
+
+        echo "Error:" . $sql . "<br>" . $conn->error;
+
+    }
+
+} 
+
+?>
+<html>
+<body>
+<p>
+         Go back to home page: <a href="https://ec2-18-220-146-166.us-east-2.compute.amazonaws.com/index.html">here</a>
+ </p>
+</body>
+</html>
