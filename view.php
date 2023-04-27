44cb2111307c98d1cac8f9a33a09020d03538e62
Rey276 <65110492+Rey276@users.noreply.github.com>
2023-04-27 01:23:22 -0700

    Update view.php

--- view.php
+++ view.php
@@ -1,53 +1,35 @@
 <?php 
-
 include "config.php";
 
 $sql = "SELECT * FROM Users";
-
 $result = $conn->query($sql);
 
 ?>
 
 <!DOCTYPE html>
-
 <html>
-
 <head>
 
     <title>View Page</title>
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
 </head>
-
 <body>
 
     <div class="container">
-
         <h2>users</h2>
-
-<table class="table">
+    <table class="table">
 
     <thead>
-
         <tr>
-
         <th>ID</th>
-
         <th>First Name</th>
-
         <th>Last Name</th>
-
         <th>Gender</th>
-
         <th>Email</th>
-
- <th>Phone</th>
-
+        <th>Phone</th>
         <th>Action</th>
-
-    </tr>
-
+        </tr>
     </thead>
 
     <tbody> 
@@ -55,44 +37,30 @@ $result = $conn->query($sql);
         <?php
 
             if ($result->num_rows > 0) {
-
                 while ($row = $result->fetch_assoc()) {
-
-        ?>
-
+                    ?>
                     <tr>
-
                     <td><?php echo $row['id']; ?></td>
-
                     <td><?php echo $row['Firstname']; ?></td>
-
                     <td><?php echo $row['Lastname']; ?></td>
-
                     <td><?php echo $row['Gender']; ?></td>
-
-      <td><?php echo $row['Email']; ?></td>
-
+                    <td><?php echo $row['Email']; ?></td>
                     <td><?php echo $row['Phone']; ?></td>
-
-                    <td><a class="btn btn-info" href="update.php?id=<?php echo $row['id']; ?>">Edit</a>&nbsp;<a class="
-btn btn-danger" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
-
+                        
+                    <td><a class="btn btn-info" href="update.php?id=<?php echo $row['id']; ?>">Edit</a>&nbsp;<a class="btn btn-danger" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
                     </tr>                       
-
-        <?php       }
-
+                    <?php       
+                }
             }
-
         ?>                
 
     </tbody>
-
-</table>
-
+    </table>
     </div> 
+    
       <p>
          Go back to home page: <a href="https://ec2-18-220-146-166.us-east-2.compute.amazonaws.com/index.html">here</a>
       </p>
+    
 </body>
-
 </html>
